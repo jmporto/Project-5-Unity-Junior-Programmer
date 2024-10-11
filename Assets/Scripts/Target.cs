67bcdbda0f8a16a29b86bd78782f6b39c42ace1b
João Marcelo Silva <jmporto@outlook.com>
2024-10-11 20:50:45 -0300

    [Feature]: Lesson 5.2 - Keeping Score
    6- Assign a point value to each target
    7- Add a Particle explosion

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,6 +11,8 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
     private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     void Start()
     {
@@ -40,7 +42,9 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        Instantiate(explosionParticle, transform.position,
+            explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
