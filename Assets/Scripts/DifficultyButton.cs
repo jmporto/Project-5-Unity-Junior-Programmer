9b58ea29c8a8a8863c5e0da6e02688a4451ff0e2
João Marcelo Silva <jmporto@outlook.com>
2024-10-14 01:54:45 -0300

    [Feature]: Lesson 5.4 - What’s the Difficulty?
    2- Add a DifficultyButton script

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();        
+    }
+
+    void Update()
+    {
+        
+    }
+}
